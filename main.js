718e3773e206deac63d756854debe52463aaf0d2
AndreaSdr70 <andrea@sdringola.it>
2024-07-11 00:28:43 +0200

    Selfwork Operatori 2

---
+++ main.js
@@ -0,0 +1,14 @@
+let anno_corrente = 2024;
+let anno_di_nascita = 1970;
+
+let eta = anno_corrente - anno_di_nascita;
+
+let cento = 100 - eta;
+
+console.log (" Hai " + eta + " anni e ti mancano " + cento + " anni per compierne 100 ");
+
+
+
+
+
+
